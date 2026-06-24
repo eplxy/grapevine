a87a47cd79ebd2ae04764ed284a3742d6e955fd5
eplxy <steven.dvlam@gmail.com>
2026-06-24 08:50:25 -0400

    Setup Tanstack router and query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,19 @@
+import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { RouterProvider } from "@tanstack/react-router"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-
 import "./index.css"
-import App from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { queryClient, router } from "./router"
+import { QueryClientProvider } from "@tanstack/react-query"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
 )
