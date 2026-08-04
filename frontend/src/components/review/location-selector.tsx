c7ffd0220744c2880220b6714122730dccf5e2c5
eplxy <steven.dvlam@gmail.com>
2026-08-04 07:51:24 -0400

    Update location-selector.tsx

--- frontend/src/components/review/location-selector.tsx
+++ frontend/src/components/review/location-selector.tsx
@@ -1,14 +1,12 @@
 import type { LocationAutocompleteSuggestion } from "@/models/models"
 import { useState } from "react"
 import LocationAutocomplete from "./location-autocomplete"
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
-import { Button } from "../ui/button"
 
 export type LocationSelectorProps = {}
 
 export type LocationModel = {} & LocationAutocompleteSuggestion
 
-export default function LocationSelector(props: LocationSelectorProps) {
+export default function LocationSelector() {
   const [location, setLocation] = useState<LocationModel>()
 
   return (
